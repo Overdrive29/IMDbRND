84d221d6ffee1274028ec65f53e7b0283605cdb9
OverDrive29 <forspam02@gmail.com>
2021-07-26 14:59:48 +0300

    Added simple telegram bot

---
+++ IMBDTgBot/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace IMBDTgBot
+{
+    class Program
+    {
+        static string token = "888105801:AAGPkc1p3utwmntbz79NtzXC-v4qujxKZfg";
+        static ITelegramBotClient botClient;
+
+        static void Main()
+        {
+            botClient = new TelegramBotClient(token);
+
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text != null)
+            {
+                using (imdbContext context = new imdbContext()) 
+                {
+                    Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+                    Console.WriteLine(e.Message.Text);
+                    Random rnd = new Random();
+                    var titles = context.Titles.Where(x => x.TitleType == "movie").ToList();
+                    string title = titles[rnd.Next(0, titles.Count)].Tconst;
+
+                    await botClient.SendTextMessageAsync(
+                      chatId: e.Message.Chat,
+                      text: "https://www.imdb.com/title/"+title
+
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ IMBDTgBot/Title.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace IMBDTgBot
+{
+    public partial class Title
+    {
+        public int Id { get; set; }
+        public string Tconst { get; set; }
+        public string TitleType { get; set; }
+        public string PrimaryTitle { get; set; }
+        public string OriginalTitle { get; set; }
+        public bool IsAdult { get; set; }
+        public int? StartYear { get; set; }
+        public int? EndYear { get; set; }
+        public int? RuntimeMinutes { get; set; }
+        public string Genres { get; set; }
+    }
+}
